33aa98e2e87cc3a22db5ee8f09238d94b369def4
Casen Ward <casenward@gmail.com>
2025-11-11 22:32:12 -0500

    Initial commit: Spring Boot REST API Server

---
+++ src/main/java/com/SoftwareTools/RestAPI_Server/RestApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.SoftwareTools.RestAPI_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/SoftwareTools/RestAPI_Server/controllers/Controller.java
@@ -0,0 +1,9 @@
+package com.SoftwareTools.RestAPI_Server.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Controller {
+    
+}
